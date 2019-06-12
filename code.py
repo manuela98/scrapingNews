1f7d6286fd508cd9bfda6b957c8be5f973442357
manuela98 <manujarare@hotmail.com>
2019-06-12 17:06:02 -0500

    update

--- code.py
+++ code.py
@@ -1,45 +1,45 @@
 import requests
 from bs4 import BeautifulSoup
-
+import codecs
 
 categories = ['antioquia','colombia','internacional','negocios','deportes','opinion']
 
 
 
-def getTitles(page,fileTitles):
+def getTitles(page,f):
     
     '''Get the titles of a page in a file'''
-    
+
+  
     soup = BeautifulSoup(page.text, 'html.parser')
     articulos = soup.find_all('article')
     for articulo in articulos:
         try:
-            titulo = articulo.span.text.encode('latin-1','ignore')
-            fileTitles.write(titulo)
-            fileTitles.write('\n')
+            titulo = articulo.span.text
+            f.write(titulo.encode('latin1').decode('utf-8'))
+            f.write('\n')
         except: 
             pass
     
-    return fileTitles
+    return 0
 
 
 def MenuSelection(categories):
 
-    '''Request the menu page and get the titles'''
-
-    file = open('titles.txt','w+')
+    """Request the menu page and get the titles"""
+    f = open('titles.txt','w+')
     for i in categories:
         print('https://www.elcolombiano.com/'+i)
         page = requests.get('https://www.elcolombiano.com/'+i)
-        getTitles(page,file)
-    file.close()
+        getTitles(page,f)
+    f.close()
     return 0
 
 def FileProcessing(fileName):
     
-    '''remove the duplications and order the news'''
-   
-    data=file(fileName).readlines()
+    """Remove the duplications and order the news"""
+    file = open(fileName)
+    data=file.readlines()
     data = set(data)
     data = sorted(data)
     fileFiltered = open('filteredTitles.txt','w+')
